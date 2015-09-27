dc447e2e7c3a2497eb0e60f433403076f84db5e3
sirjklay <jklay@ucla.edu>
2015-09-27 15:23:02 -0700

    Programming Assignment 2 - sirjklay
    Programming assignment 2 by sirjklay for caching the inverse of a
    matrix; includes the makeCacheMatrix and cacheSolve functions.

--- cachematrix.R
+++ cachematrix.R
@@ -33,7 +33,7 @@ cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         inv <- x$getinverse()
         if (!is.null(inv)) {
-                message("retrieving cached data")
+                message("getting cached data")
                 return(inv)
         }
         mat <- x$get()
